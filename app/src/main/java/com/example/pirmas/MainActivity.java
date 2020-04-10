a6801e64e4a17cd6947ee6621cc27fc80191d93b
Erika <erika.semigradskyte@knf.stud.vu.lt>
2020-04-10 22:22:05 +0300

    Pridetas mygtukas, pakeiciantis teksta

--- app/src/main/java/com/example/pirmas/MainActivity.java
+++ app/src/main/java/com/example/pirmas/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.pirmas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView textView = (TextView) findViewById(R.id.textView);
+        final Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                textView.setText("Laba diena, su vištiena.");
+            }
+        });
     }
 }
